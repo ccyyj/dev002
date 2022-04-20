a5ce007d76b01e0e849f486717a7df5790ce2dd3
cyj <2256818165@qq.com>
2022-04-20 14:33:03 +0800

    SpringBoot

--- 08-SpringBoot-02-Config-File/src/main/java/com/fc/controller/UserController.java
+++ 08-SpringBoot-02-Config-File/src/main/java/com/fc/controller/UserController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyController {
+public class UserController {
     @RequestMapping("hello")
     public String hello() {
-        return "Hello";
+        return "猪脚饭，鸡公煲，凉皮，泡面加火腿肠加蛋";
     }
 }
--- 08-SpringBoot-1-Starter/src/main/java/com/fc/controller/UserController.java
+++ 08-SpringBoot-1-Starter/src/main/java/com/fc/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.fc.controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyController {
+public class UserController {
     @RequestMapping("hello")
     public String hello() {
-        return "Hello";
+        return "蛋白棒、无糖豆浆、八宝粥";
     }
 }
